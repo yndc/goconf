38c4355b072fec7882c298e9e4d9a5817b0222ed
yondercode <yondercode@gmail.com>
2022-01-22 00:48:48 +0700

    added files

---
+++ pkg/schema/schema.go
@@ -0,0 +1,14 @@
+package recon
+
+import "reflect"
+
+type Schema struct {
+	fields []Field
+}
+
+func 
+
+// GenerateSchema generates a new schema from the given type
+func GenerateSchema(source reflect.Type) *Schema {
+	return nil
+}
