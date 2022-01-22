4b673da77c3ad2ff7bf1e4c2da7e892ddae77600
yondercode <yondercode@gmail.com>
2022-01-22 18:58:43 +0700

    checkpoint

--- pkg/schema/generate.go
+++ pkg/schema/generate.go
@@ -1,8 +1,27 @@
 package schema
 
-import "reflect"
+import (
+	"reflect"
+
+	"github.com/yndc/recon/pkg/utils"
+)
 
 // GenerateSchema generates a new schema from the given type
-func GenerateSchema(source reflect.Type) *Schema {
+func GenerateSchema(source interface{}) *Schema {
+	schema := &Schema{}
+	schema.fields = make([]Field, 0)
+	utils.TraverseStructType(source, func(path *utils.Path, field reflect.StructField) {
+		fieldSchema := Field{}
+		fieldType := field.Type
+		if fieldType.Kind() == reflect.Ptr {
+			fieldSchema.required = true
+			fieldType = field.Type.Elem()
+		}
+
+		fieldSchema.valueType = fieldType
+		fieldSchema.path = path
+
+		fieldType.
+	})
 	return nil
 }
